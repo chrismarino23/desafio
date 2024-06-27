30fc1ce8e1ccf70e86e80c2a5262a1e642140ec9
Crystopher Marino <chrismarino23@gmail.com>
2024-06-26 22:16:33 -0300

    desafio do dia 26-06

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-/*
-Inserir preço de custo 
-Somar ICMS
-Mostrar preço calculado
+const vlrTotal = parseFloat(prompt("Informe o valor total: "));
 
-Deve ser inserido pelo user e saído em alert
-*/
-// const taxa = 0.15;
-
-const calcTaxa = (v) => {
-  return (v * taxa) / 100;
-};
-
-const fgtsFinal = (v) => {
-  return v - calcTaxa(v);
-};
-
-alert(
-  "Bem vindo ao saque empréstimo FGTS!\nPara saber quanto você pode sacar e qual a taxa calculada encima do valor total, por favor clique em OK e siga os passos seguintes!"
-);
-
-const vlrFGTS = parseFloat(prompt("Insira o valor do seu FGTS total: "));
-
-const taxa = parseInt(prompt("Informe a porcentagem da taxa do Banco: "));
-
-const informaUser = () => {
-  alert(
-    `O seu FGTS Bruto é de R$ ${vlrFGTS}\nA taxa para esse cálculo será de: R$ ${calcTaxa(
-      vlrFGTS
-    )}\nE o FGTS líquido disponível para saque será de: R$ ${fgtsFinal(
-      vlrFGTS
-    )}`
+const calculaParcela = (vlr) => {
+  let qtdParcela = parseInt(
+    prompt("Informe a quantidade de parcelas desejadas para cáculo: ")
   );
+
+  for (let i = 1; i <= qtdParcela; i++) {
+    if (i === 1) {
+      vlr = vlr / qtdParcela;
+    }
+
+    console.log(`O valor da ${i}a parcela é de R$ ${vlr.toFixed(2)}`);
+  }
 };
 
-informaUser();
+calculaParcela(vlrTotal);
